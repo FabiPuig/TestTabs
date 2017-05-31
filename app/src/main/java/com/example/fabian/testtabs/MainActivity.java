783405ea5ef930cf68e2e5d2a1643abf57db1db0
Fabian <20464654j@iespoblenou.org>
2017-05-31 14:01:17 +0200

    Cambiamos el texto del tab por un View personalizado. Añadido un 4o tab.

--- app/src/main/java/com/example/fabian/testtabs/MainActivity.java
+++ app/src/main/java/com/example/fabian/testtabs/MainActivity.java
@@ -1,16 +1,22 @@
 package com.example.fabian.testtabs;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.method.ScrollingMovementMethod;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TabHost;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TabHost tabHost;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,24 +25,33 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        TabHost tabHost=(TabHost)findViewById(R.id.tabHost);
+        tabHost=(TabHost)findViewById(R.id.tabHost);
         tabHost.setup();
 
-        TabHost.TabSpec spec1=tabHost.newTabSpec("Tab 1");
+        View tabview1 = createTabView( tabHost.getContext(), "10", "Tab 1");
+        TabHost.TabSpec spec1= tabHost.newTabSpec("Tab 1");
+        spec1.setIndicator( tabview1 );
         spec1.setContent(R.id.tab1);
-        spec1.setIndicator("Tab 1");
 
+        View tabview2 = createTabView( tabHost.getContext(), "20", "Tab 2");
         TabHost.TabSpec spec2=tabHost.newTabSpec("Tab 2");
-        spec2.setIndicator("Tab 2");
+        spec2.setIndicator( tabview2 );
         spec2.setContent(R.id.tab2);
 
+        View tabview3 = createTabView( tabHost.getContext(), "30", "Tab 3");
         TabHost.TabSpec spec3=tabHost.newTabSpec("Tab 3");
-        spec3.setIndicator("Tab 3");
+        spec3.setIndicator( tabview3 );
         spec3.setContent(R.id.tab3);
+
+        View tabview4 = createTabView( tabHost.getContext(), "40", "Tab 4");
+        TabHost.TabSpec spec4=tabHost.newTabSpec("Tab 4");
+        spec4.setIndicator( tabview4 );
+        spec4.setContent(R.id.tab4);
 
         tabHost.addTab(spec1);
         tabHost.addTab(spec2);
         tabHost.addTab(spec3);
+        tabHost.addTab(spec4);
     }
 
     @Override
@@ -59,5 +74,25 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /** Crea el view que ira en un tab
+     *
+     * @param context
+     * @param text1
+     * @param text2
+     * @return
+     */
+    private View createTabView(final Context context, final String text1, final String text2) {
+
+        final View view = LayoutInflater.from(context).inflate(R.layout.tabs_bg, null);
+
+        final TextView tv1 = (TextView) view.findViewById(R.id.tabsTv1);
+        final TextView tv2 = (TextView) view.findViewById(R.id.tabsTv2);
+
+        tv1.setText( text1 );
+        tv2.setText( text2 );
+
+        return view;
     }
 }
